f5148e5b97ce9be1dd2b3e90800c49f78abe746c
KH1473 <2210370@ecc.ac.jp>
2022-11-04 13:51:42 +0900

    piiza contexts

--- Source/PizzaConstants.h
+++ Source/PizzaConstants.h
@@ -8,4 +8,30 @@
   //DEBUG_EFFECT以下の" "の中身を変えると違うものを呼び出せます
   //参考に"Data/Effect/03_Hanmado01/attack.efk"を入れてます
 
+//SceneTitle.cpp
+#define TITLE_SPRITE "Data/Sprite/Title.png" //タイトル描画
+
+//SceneLoading.cpp
+#define LOADING_SPRITE "Data/Sprite/LoadingIcon.png" //ローディングアイコン
+
+//SceneSousa.cpp
+#define SOUSA_SPRITE "Data/Sprite/sousa.png" //操作描画
+#define SOUSA_SSMP "Data/Sprite/MP.png" //マウス操作(緑) もとから見える緑のほう
+#define SOUSA_SSRed "Data/Sprite/RED.png" //マウス操作(赤)　触れると反応するほう
+#define SOUSA_SSMark "Data/Sprite/mark2.png" //操作説明の回転しているやつ
+
+//SceneGame.cpp
+#define GAME_MP "Data/Sprite/MP.png"
+#define GAME_Red "Data/Sprite/RED.png"
+#define GAME_Back "Data/Sprite/Space.png"
+
+//SceneScore.cpp
+#define SCORE_SPRITE "Data/Sprite/Score.png"
+#define SCORE_TEXT "Data/Font/font1.png"
+
+//EnemySlime.cpp
+#define PIZZAMODEL "Data/Model/PIZZA/PIZZA.mdl"
+
+//Player.cpp
+#define PLAYERMODEL "Data/Model/Jammo/Jammo.mdl"
 #define DEBUG_EFFECT "Data/Effect/Hit.efk"
